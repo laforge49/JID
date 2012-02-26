57b9e99b23ddfa9803d8bd9fc80c4022fd726278
Bill LaForge <laforge49@gmail.com>
2012-02-26 11:59:25 +0530

    BooleanTest now tests a BooleanJid in a JidJid

--- src/test/java/org/agilewiki/jid/scalar/BooleanTest.java
+++ src/test/java/org/agilewiki/jid/scalar/BooleanTest.java
@@ -10,6 +10,7 @@ import org.agilewiki.jactor.components.factory.NewActor;
 import org.agilewiki.jid.JidFactories;
 import org.agilewiki.jid.requests.CopyJID;
 import org.agilewiki.jid.requests.GetSerializedLength;
+import org.agilewiki.jid.requests.ResolvePathname;
 
 public class BooleanTest extends TestCase {
     public void test() {
@@ -35,6 +36,16 @@ public class BooleanTest extends TestCase {
             assertFalse(BooleanJid.getValueReq.send(future, boolean1));
             assertTrue(BooleanJid.getValueReq.send(future, boolean2));
             assertTrue(BooleanJid.getValueReq.send(future, boolean3));
+
+            NewActor newJidJid = new NewActor(JidFactories.JID_JID_TYPE);
+            JCActor jidJid1 = newJidJid.send(future, factory);
+            SetValue sjvb = JidJid.setValueReq(JidFactories.BOOLEAN_JID_TYPE);
+            sjvb.send(future, jidJid1);
+            JCActor rpa = (new ResolvePathname("$")).send(future, jidJid1);
+            assertFalse(BooleanJid.getValueReq.send(future, rpa));
+            BooleanJid.setValueReq(true).send(future, rpa);
+            rpa = (new ResolvePathname("$")).send(future, jidJid1);
+            assertTrue(BooleanJid.getValueReq.send(future, rpa));
 
         } catch (Exception e) {
             e.printStackTrace();
