18f127e30026801fd5a45b90ff9b211b7a21a896
Bill LaForge <laforge49@gmail.com>
2012-02-28 09:59:34 +0530

    Tuples are comparable on their first element; equals redefined as first elements are equal

--- src/main/java/org/agilewiki/jid/tuple/TupleJid.java
+++ src/main/java/org/agilewiki/jid/tuple/TupleJid.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2012 Bill La Forge
+ *
+ * This file is part of AgileWiki and is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License (LGPL) as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or navigate to the following url http://www.gnu.org/licenses/lgpl-2.1.txt
+ *
+ * Note however that only Scala, Java and JavaScript files are being covered by LGPL.
+ * All other files are covered by the Common Public License (CPL).
+ * A copy of this license is also included and can be
+ * found as well at http://www.opensource.org/licenses/cpl1.0.txt
+ */
 package org.agilewiki.jid.tuple;
 
 import org.agilewiki.jactor.bind.Internals;
@@ -10,10 +33,15 @@ import org.agilewiki.jid.ReadableBytes;
 import org.agilewiki.jid.Util;
 import org.agilewiki.jid.requests.GetJIDComponent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Holds an array of actors.
  */
-public class TupleJid extends JID {
+public class TupleJid
+        extends JID
+        implements Comparable<TupleJid> {
     /**
      * An array of actor types, one for each element in the tuple.
      */
@@ -180,6 +208,8 @@ public class TupleJid extends JID {
     @Override
     public JCActor resolvePathname(Internals internals, String pathname)
             throws Exception {
+        ArrayList x = new ArrayList();
+        Arrays.binarySearch(x.toArray(), "");
         if (pathname.length() == 0) {
             return thisActor;
         }
@@ -201,5 +231,59 @@ public class TupleJid extends JID {
         if (s == pathname.length())
             return jid.thisActor;
         return jid.resolvePathname(internals, pathname.substring(s + 1));
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     * <p/>
+     * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
+     * <tt>y.compareTo(x)</tt> throws an exception.)
+     * <p/>
+     * <p>The implementor must also ensure that the relation is transitive:
+     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+     * <tt>x.compareTo(z)&gt;0</tt>.
+     * <p/>
+     * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
+     * all <tt>z</tt>.
+     * <p/>
+     * <p>It is strongly recommended, but <i>not</i> strictly required that
+     * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
+     * class that implements the <tt>Comparable</tt> interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     * <p/>
+     * <p>In the foregoing description, the notation
+     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+     * <i>expression</i> is negative, zero or positive.
+     *
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     *         is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public int compareTo(TupleJid o) {
+        Comparable e0 = (Comparable) tuple[0];
+        return e0.compareTo(o.tuple[0]);
+    }
+
+    @Override
+    public boolean equals(Object v) {
+        if (v == null)
+            return false;
+        if (!(v instanceof TupleJid))
+            return false;
+        TupleJid tj = (TupleJid) v;
+        return tuple[0].equals(tj.tuple[0]);
     }
 }
