a092537924d2494c474a5bb9e144f65a184e2eb4
Bill LaForge <laforge49@gmail.com>
2012-03-06 14:24:19 +0530

    New ActorTypes component.

--- src/main/java/org/agilewiki/jid/tuple/ActorTypes.java
+++ src/main/java/org/agilewiki/jid/tuple/ActorTypes.java
@@ -23,11 +23,38 @@
  */
 package org.agilewiki.jid.tuple;
 
-import org.agilewiki.jactor.bind.ConcurrentRequest;
+import org.agilewiki.jactor.bind.ConcurrentMethodBinding;
+import org.agilewiki.jactor.bind.RequestReceiver;
+import org.agilewiki.jactor.components.Component;
 
 /**
- * Returns an array of actor types.
+ * Base class for defining array types of tuples.
  */
-public class GetActorTypes extends ConcurrentRequest<String[]> {
-    public final static GetActorTypes req = new GetActorTypes();
+abstract public class ActorTypes extends Component {
+
+    /**
+     * Returns an array of actor types.
+     *
+     * @return An array of actor types.
+     */
+    abstract protected String[] ats();
+
+    /**
+     * Bind request classes.
+     *
+     * @throws Exception Any exceptions thrown while binding.
+     */
+    @Override
+    public void bindery() throws Exception {
+        thisActor.bind(
+                GetActorTypes.class.getName(),
+                new ConcurrentMethodBinding<GetActorTypes, String[]>() {
+                    @Override
+                    public String[] concurrentProcessRequest(RequestReceiver requestReceiver,
+                                                             GetActorTypes request)
+                            throws Exception {
+                        return ats();
+                    }
+                });
+    }
 }
