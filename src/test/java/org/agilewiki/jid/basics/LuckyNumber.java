1d1e66dcca66c46f6d95dd161d5f2f3b5e3c50ea
Bill LaForge <laforge49@gmail.com>
2012-04-24 11:00:22 +0530

    New LuckyNumberTest

--- src/test/java/org/agilewiki/jid/basics/LuckyNumber.java
+++ src/test/java/org/agilewiki/jid/basics/LuckyNumber.java
@@ -2,17 +2,17 @@ package org.agilewiki.jid.basics;
 
 import org.agilewiki.jactor.Mailbox;
 import org.agilewiki.jactor.RP;
-import org.agilewiki.jid.Jid;
+import org.agilewiki.jid.scalar.flens.integer.IntegerJid;
 
-public class HelloWorld extends Jid implements Main {
-    public HelloWorld(final Mailbox mailbox) {
+public class LuckyNumber extends IntegerJid implements Main {
+    public LuckyNumber(final Mailbox mailbox) {
         super(mailbox);
     }
 
     @Override
     protected void processRequest(Object request, RP rp) throws Exception {
         if (request.getClass() == Proc.class) {
-            System.out.println("Hello world!");
+            System.out.println("Your lucky number is " + getValue());
             rp.processResponse(null);
             return;
         }
---
+++ src/test/java/org/agilewiki/jid/basics/LuckyNumberTest.java
@@ -0,0 +1,36 @@
+package org.agilewiki.jid.basics;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor.*;
+import org.agilewiki.jactor.factory.JAFactory;
+import org.agilewiki.jid.GetSerializedBytes;
+import org.agilewiki.jid.ResolvePathname;
+import org.agilewiki.jid.scalar.flens.integer.SetInteger;
+import org.agilewiki.jid.scalar.vlens.actor.RootJid;
+import org.agilewiki.jid.scalar.vlens.actor.SetActor;
+
+public class LuckyNumberTest extends TestCase {
+    public void test()
+            throws Exception {
+        MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(1);
+        Mailbox mailbox = mailboxFactory.createMailbox();
+        JAFuture future = new JAFuture();
+        JAFactory factory = new JAFactory(mailbox);
+        factory.defineActorType("lucky number", LuckyNumber.class);
+
+        RootJid root = new RootJid(mailbox);
+        root.setParent(factory);
+        (new SetActor("lucky number")).send(future, root);
+        Actor a = (new ResolvePathname("0")).send(future, root);
+        (new SetInteger(7)).send(future, a);
+        byte[] rootBytes = GetSerializedBytes.req.send(future, root);
+
+        RootJid root2 = new RootJid(mailbox);
+        root2.setParent(factory);
+        root2.load(rootBytes);
+        Actor a2 = (new ResolvePathname("0")).send(future, root2);
+        Proc.req.send(future, a2);
+
+        mailboxFactory.close();
+    }
+}
