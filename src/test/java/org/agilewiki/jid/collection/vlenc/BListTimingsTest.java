2d04330d3182e4b188d9b57391cbe4fa9c034dd4
Bill La Forge <laforge49@gmail.com>
2012-09-29 09:36:09 +0530

    BListJid.iAddBytes finished

---
+++ src/test/java/org/agilewiki/jid/collection/vlenc/BListTimingsTest.java
@@ -0,0 +1,53 @@
+package org.agilewiki.jid.collection.vlenc;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor.JAMailboxFactory;
+import org.agilewiki.jactor.Mailbox;
+import org.agilewiki.jactor.MailboxFactory;
+import org.agilewiki.jactor.factory.JAFactory;
+import org.agilewiki.jid.JidFactories;
+import org.agilewiki.jid.scalar.flens.integer.IntegerJid;
+import org.agilewiki.jid.scalar.flens.integer.IntegerJidFactory;
+
+public class BListTimingsTest extends TestCase {
+    public void test() {
+
+        int s = 100000;
+        int r = 1000;
+
+        MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(1);
+        try {
+            Mailbox mailbox = mailboxFactory.createMailbox();
+            JAFactory factory = new JAFactory();
+            factory.initialize(mailbox);
+            (new JidFactories()).initialize(mailbox, factory);
+            BListJidFactory intListFactory = new BListJidFactory("il", IntegerJidFactory.fac);
+            factory.registerActorFactory(intListFactory);
+            BListJid intList1 = (BListJid) factory.newActor("il");
+            int i = 0;
+            while (i < 100000) {
+                intList1.iAdd(-1);
+                IntegerJid ij0 = (IntegerJid) intList1.iGet(-1);
+                ij0.setValue(i);
+                i += 1;
+            }
+            intList1.getSerializedBytes();
+            long t0 = System.currentTimeMillis();
+            int j = 0;
+            while (j < r) {
+                BListJid intList2 = (BListJid) intList1.copyJID(mailbox);
+                intList1.iAdd(s/2);
+                intList2.getSerializedBytes();
+                j += 1;
+            }
+            long t1 = System.currentTimeMillis();
+            System.out.println("list size = " + s);
+            long rt = t1 - t0;
+            System.out.println("total run time = " + rt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            mailboxFactory.close();
+        }
+    }
+}
