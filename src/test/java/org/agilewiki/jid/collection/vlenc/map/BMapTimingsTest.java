c6a3b31c6472879012f18b3923f8b4b14cba043a
Bill La Forge <laforge49@gmail.com>
2012-10-15 07:53:44 +0530

    updates a serialized byte array of a map with 100,000 entries in half a millisecond

--- src/test/java/org/agilewiki/jid/collection/vlenc/map/BMapTimingsTest.java
+++ src/test/java/org/agilewiki/jid/collection/vlenc/map/BMapTimingsTest.java
@@ -1,4 +1,4 @@
-package org.agilewiki.jid.collection.vlenc;
+package org.agilewiki.jid.collection.vlenc.map;
 
 import junit.framework.TestCase;
 import org.agilewiki.jactor.JAMailboxFactory;
@@ -7,17 +7,17 @@ import org.agilewiki.jactor.MailboxFactory;
 import org.agilewiki.jactor.factory.JAFactory;
 import org.agilewiki.jid.JidFactories;
 import org.agilewiki.jid.scalar.flens.integer.IntegerJid;
-import org.agilewiki.jid.scalar.flens.integer.IntegerJidFactory;
 
-public class BListTimingsTest extends TestCase {
-    public void test() {
+public class BMapTimingsTest extends TestCase {
+    public void test1() throws Exception {
 
-        int s = 1000;
-        int r = 1000;
-        // list size = 100,000
-        // repetitions = 10,000
-        // total run time (milliseconds) = 2394
-        // time per update (microseconds) = 239
+        int s = 100000;
+        int r = 10000;
+
+        //map size = 100000
+        //repetitions = 10000
+        //total run time (milliseconds) = 4775
+        //time per update (microseconds) = 477
 
         MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(1);
         try {
@@ -25,34 +25,32 @@ public class BListTimingsTest extends TestCase {
             JAFactory factory = new JAFactory();
             factory.initialize(mailbox);
             (new JidFactories()).initialize(mailbox, factory);
-            BListJidFactory intListFactory = new BListJidFactory("il", IntegerJidFactory.fac);
-            factory.registerActorFactory(intListFactory);
-            BListJid intList1 = (BListJid) factory.newActor("il");
+            BMapJid<Integer, IntegerJid> m1 = (BMapJid) factory.newActor(JidFactories.INTEGER_INTEGER_BMAP_JID_TYPE);
             int i = 0;
             while (i < s) {
-                intList1.iAdd(-1);
-                IntegerJid ij0 = (IntegerJid) intList1.iGet(-1);
+                m1.kMake(i);
+                IntegerJid ij0 = m1.kGet(i);
                 ij0.setValue(i);
                 i += 1;
             }
-            intList1.getSerializedBytes();
+            m1.getSerializedBytes();
             long t0 = System.currentTimeMillis();
             int j = 0;
+            i = s / 2;
             while (j < r) {
-                BListJid intList2 = (BListJid) intList1.copyJID(mailbox);
-                intList1.iAdd(s / 2);
-                intList2.getSerializedBytes();
+                BMapJid m2 = (BMapJid) m1.copyJID(mailbox);
+                IntegerJid ij0 = m1.kGet(i);
+                ij0.setValue(-i);
+                m2.getSerializedBytes();
                 j += 1;
             }
             long t1 = System.currentTimeMillis();
-            System.out.println("list size = " + s);
+            System.out.println("map size = " + s);
             System.out.println("repetitions = " + r);
             long rt = t1 - t0;
             System.out.println("total run time (milliseconds) = " + rt);
             long tpu = rt * 1000L / r;
             System.out.println("time per update (microseconds) = " + tpu);
-        } catch (Exception e) {
-            e.printStackTrace();
         } finally {
             mailboxFactory.close();
         }
